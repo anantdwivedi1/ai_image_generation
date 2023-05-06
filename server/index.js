a63a762993a204b48efc92aa760113dd3a86ae54
Anant Dwivedi <anantdwivedi912@gmail.com>
2023-05-06 23:26:37 +0530

    added loader ,card , formatfield

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+const path = require('path')
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
+
+// for static files - hosting purpose
+app.use(express.static(path.join(__dirname , './client/dist')));
+
+app.get('*' , function(req,res){
+  res.sendFile(path.join(__dirname ,"./client/dist/index.html"))
+});
+
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  } 
+  const x =10;
+};
+
+startServer();
